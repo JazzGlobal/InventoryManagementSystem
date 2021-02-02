bf1cf65fa948964dc60e12cd8202060b4ff8bc22
JazzGlobal <waymer147@gmail.com>
2021-02-02 13:17:24 -0500

    Added.

---
+++ InventoryManagementSystem/EventManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public class EventManager
+    {
+        public delegate void AddPartAction(Part part, bool outsourced);
+        public static event AddPartAction OnAddPart;
+    
+        public static void FireAddPart(Part part, bool outsourced)
+        {
+            OnAddPart(part, outsourced);
+        }
+    }
+}
