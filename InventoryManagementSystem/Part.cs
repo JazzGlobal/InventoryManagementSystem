c5782141f1690282bdab9815d760fcea1ea5efd0
JazzGlobal <waymer147@gmail.com>
2021-02-01 22:48:09 -0500

    Added required properties.

---
+++ InventoryManagementSystem/Part.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public abstract class Part
+    {
+        public int PartID;
+        public string Name;
+        public decimal Price;
+        public int InStock;
+        public int Min;
+        public int Max;
+    }
+}
