6827878018cd3a461e1ae35418716039715dc1fa
JazzGlobal <waymer147@gmail.com>
2021-02-01 22:52:48 -0500

    Add template methods and properties.

---
+++ InventoryManagementSystem/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem
+{
+    public class Product
+    {
+        public BindingList<Part> AssociatedParts;
+        public int ProductID;
+        public string Name;
+        public decimal Price;
+        public int InStock;
+        public int Min;
+        public int Max;
+
+        public void addAssociatedPart(Part part)
+        {
+            throw new NotImplementedException();
+        }
+        public bool removeAssociatedPart(int index)
+        {
+            throw new NotImplementedException(); 
+        }
+        public Part lookupAssociatedPart(int index)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
